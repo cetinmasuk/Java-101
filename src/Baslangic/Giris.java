0383c7f96751473782b6ad0ec3249ed6c776aaf0
masukcetin <56764157+cetinmasuk@users.noreply.github.com>
2021-06-26 23:56:17 +0300

    Kodluyoruz-Java Kamp

---
+++ src/Baslangic/Giris.java
@@ -0,0 +1,23 @@
+package Baslangic;
+
+public class Giris {
+    //Ana metotu yazmak için kısa  yollar: psvm || main kullanılır.
+    public static void main(String[] args) {
+
+        //Ekrana yazdırmanın kısayolu: sout
+
+        System.out.println("Merhaba ");
+        System.out.println("Merhaba "); //Noktalı virgülün bittiği alan statmen denir.
+        int a= 10, b=8, c =10;
+
+        int sonuc = a+b+c;
+
+        String  isim = "Masuk";
+        String soyisim = "Cetin";
+
+        System.out.println("Toplam Değer: " + sonuc );
+
+        System.out.println("Adınız: " + isim  +  "\n " + " Soyadınız: " + soyisim);
+
+    }
+}
